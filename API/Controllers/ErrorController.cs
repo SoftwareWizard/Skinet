0a512c4315a1940f8f87c9bce6d5f94e3fd5d439
Simon Bauer <simon.bauer@doka.com>
2021-07-01 18:59:30 -0500

    cleanup startup

--- API/Controllers/ErrorController.cs
+++ API/Controllers/ErrorController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
+    [Route("errors/{code}")]
+    [ApiExplorerSettings(IgnoreApi = true)]
     public class ErrorController : ApiControllerBase
     {
         public IActionResult Error(int code)
