c3d057d8f853a5a608b4dec6147eb769a480937a
Simon Bauer <simon.bauer@doka.com>
2021-07-10 07:49:25 -0500

    refactoring

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,21 @@
+import { CoreModule } from './core/core.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
-   declarations: [AppComponent, NavBarComponent],
-   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, HttpClientModule],
+   declarations: [AppComponent],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      BrowserAnimationsModule,
+      HttpClientModule,
+      CoreModule,
+   ],
    providers: [],
    bootstrap: [AppComponent],
 })
