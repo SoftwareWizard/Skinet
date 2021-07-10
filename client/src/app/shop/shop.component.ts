cc301871b5af2bc1136c06bdeb0373199b9a59ae
Simon Bauer <simon.bauer@doka.com>
2021-07-10 13:35:35 -0500

    add filter

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,6 +1,8 @@
+import { IProductBrand } from './../models/product-brand';
 import { ShopService } from './shop.service';
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from '../models/product';
+import { IProductType } from '../models/product-type';
 
 @Component({
    selector: 'app-shop',
@@ -8,12 +10,40 @@ import { IProduct } from '../models/product';
    styleUrls: ['./shop.component.scss'],
 })
 export class ShopComponent implements OnInit {
-   public products : IProduct[] = [];
+   public products: IProduct[] = [];
+   public types: IProductType[] = [];
+   public brands: IProductBrand[] = [];
+
+   public selectedBrandId: number = 1;
+   public selectedTypeId: number = 1;
 
    constructor(private shopService: ShopService) {}
 
    async ngOnInit(): Promise<void> {
-      var pagination = await this.shopService.getProducts().toPromise();
+      await this.getProducts();
+      await this.getBrands();
+      await this.getTypes();
+   }
+
+   private async getProducts(): Promise<void> {
+      let pagination = await this.shopService
+         .getProducts(this.selectedBrandId, this.selectedTypeId)
+         .toPromise();
       this.products = pagination.data;
+   }
+
+   private async getTypes(): Promise<void> {
+      this.types = await this.shopService.getTypes().toPromise();
+      this.types = [{ id: 0, name: 'All' }, ...this.types];
+   }
+
+   private async getBrands(): Promise<void> {
+      this.brands = await this.shopService.getBrands().toPromise();
+      this.brands = [{ id: 0, name: 'All' }, ...this.brands];
+   }
+
+   async onBrandSelected(brandId: number): Promise<void> {
+      this.selectedBrandId = brandId;
+      await this.getProducts();
    }
 }
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,7 +1,9 @@
 import { Observable } from 'rxjs';
 import { IPagination } from './../models/pagination';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IProductBrand } from '../models/product-brand';
+import { IProductType } from '../models/product-type';
 @Injectable({
    providedIn: 'root',
 })
@@ -10,7 +12,27 @@ export class ShopService {
 
    constructor(private http: HttpClient) {}
 
-   public getProducts(): Observable<IPagination> {
-      return this.http.get<IPagination>(`${this.baseUrl}/products`);
+   public getProducts(brandId?: number, typeId?: number): Observable<IPagination> {
+      let params = new HttpParams();
+
+      params.append('pageSize', 50);
+
+      if (brandId) {
+         params.append('typeId', brandId.toString());
+      }
+
+      if (typeId) {
+         params.append('brandId', typeId.toString());
+      }
+
+      return this.http.get<IPagination>(`${this.baseUrl}/products`, { params });
+   }
+
+   public getTypes(): Observable<IProductType[]> {
+      return this.http.get<IProductType[]>(`${this.baseUrl}/products/types`);
+   }
+
+   public getBrands(): Observable<IProductBrand[]> {
+      return this.http.get<IProductBrand[]>(`${this.baseUrl}/products/brands`);
    }
 }
