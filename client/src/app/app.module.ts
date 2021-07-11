318867fa45eedf9f572a8e9a96b80a65daee6e41
Simon Bauer <simon.bauer@doka.com>
2021-07-11 07:41:53 -0500

    add home module and component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app.routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
