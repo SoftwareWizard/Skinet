39aba58d925320552e01074e86658a683f9b9bbe
Simon Bauer <simon.bauer@doka.com>
2021-07-11 07:46:16 -0500

    add product-details component

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
 import { ProductItemComponent } from './product-item/product-item.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
-   declarations: [ShopComponent, ProductItemComponent],
+   declarations: [ShopComponent, ProductItemComponent, ProductDetailsComponent],
    imports: [CommonModule, BrowserModule, SharedModule],
    exports: [ShopComponent],
 })
