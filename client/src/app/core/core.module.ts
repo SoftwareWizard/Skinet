ace80f4f6cc49615957b285e8a8413d42c4974ed
Simon Bauer <simon.bauer@doka.com>
2021-07-12 18:25:50 -0500

    add toastr

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -5,10 +5,20 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { TestErrorComponent } from './test-error/test-error.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
    declarations: [NavBarComponent, TestErrorComponent, NotFoundComponent, ServerErrorComponent],
-   imports: [CommonModule, RouterModule],
+   imports: [
+      CommonModule,
+      RouterModule,
+      BrowserAnimationsModule,
+      ToastrModule.forRoot({
+         positionClass: 'toast-bottom-right',
+         preventDuplicates: true,
+      }),
+   ],
    exports: [NavBarComponent],
 })
 export class CoreModule {}
