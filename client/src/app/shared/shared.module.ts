fd56f8739f7f8baa603b2c5e341a412b0369e2ab
Simon Bauer <simon.bauer@doka.com>
2021-07-20 10:23:06 -0500

    implement home page

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
 import { PagerComponent } from './components/pager/pager.component';
+import { CarouselComponent, CarouselModule, SlideComponent } from 'ngx-bootstrap/carousel';
 @NgModule({
    declarations: [PagingHeaderComponent, PagerComponent],
-   imports: [CommonModule, PaginationModule.forRoot()],
-   exports: [PaginationModule, PagingHeaderComponent, PagerComponent],
+   imports: [CommonModule, PaginationModule.forRoot(), CarouselModule.forRoot()],
+   exports: [PaginationModule, PagingHeaderComponent, PagerComponent, CarouselComponent, SlideComponent],
 })
 export class SharedModule {}
