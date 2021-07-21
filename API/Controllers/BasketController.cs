73bbccab1806372121d512bb8e9217be796fa222
Simon Bauer <simon.bauer@doka.com>
2021-07-21 16:30:38 -0500

    add controller

---
+++ API/Controllers/BasketController.cs
@@ -0,0 +1,45 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _basketRepository;
+
+        public BasketController(IBasketRepository basketRepository)
+        {
+            _basketRepository = basketRepository;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<CustomerBasket>> GetBasket(string id)
+        {
+            var basket = await _basketRepository.GetBasket(id);
+            return Ok(basket ?? new CustomerBasket(id));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CustomerBasket>> UpdateBasket([FromBody] CustomerBasket basket)
+        {
+            var updatedBasket = await _basketRepository.UpdateBasket(basket);
+            return Ok(updatedBasket);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<ActionResult> DeleteBasket(string id)
+        {
+            var isDeleted = await _basketRepository.DeleteBasket(id);
+
+            return isDeleted 
+                ? Ok() 
+                : NotFound();
+        }
+    }
+}
