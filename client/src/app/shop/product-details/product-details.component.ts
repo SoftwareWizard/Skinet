68947fde78a0f849fd8ba4827d4febeaba6e2424
Simon Bauer <simon.bauer@doka.com>
2021-07-23 15:51:56 -0500

    implement product-details

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -1,8 +1,11 @@
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { IProduct } from './../../shared/models/product';
 import { ShopService } from './../shop.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BreadcrumbService } from 'xng-breadcrumb';
+import { BasketService } from 'src/app/basket/basket.service';
 @Component({
    selector: 'app-product-details',
    templateUrl: './product-details.component.html',
@@ -11,10 +14,12 @@ import { BreadcrumbService } from 'xng-breadcrumb';
 export class ProductDetailsComponent implements OnInit {
    public product: IProduct = {} as IProduct;
    private productId: number = 0;
+   public quantity: number = 1;
 
    constructor(
       private shopService: ShopService,
       private activatedRoute: ActivatedRoute,
+      private basketService: BasketService,
       private breadcrumbService: BreadcrumbService
    ) {}
 
@@ -28,6 +33,20 @@ export class ProductDetailsComponent implements OnInit {
 
    private async loadProduct() {
       this.product = await this.shopService.getProduct(this.productId).toPromise();
-      this.breadcrumbService.set('@productDetails', this.product.name)
+      this.breadcrumbService.set('@productDetails', this.product.name);
+   }
+
+   public increaseQuantity() {
+      this.quantity++;
+   }
+
+   public decreaseQuantity() {
+      if (this.quantity > 1) {
+         this.quantity--;
+      }
+   }
+
+   public async addToBasket() {
+      await this.basketService.addItemToBasket(this.product, this.quantity);
    }
 }
