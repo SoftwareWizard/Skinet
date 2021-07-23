77ed487444e7403177d10f8281073c95315810f2
Simon Bauer <simon.bauer@doka.com>
2021-07-23 17:35:27 -0500

    add EF Identiy Model

--- Core/Entities/Identity/AppUser.cs
+++ Core/Entities/Identity/AppUser.cs
@@ -1,15 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Core.Entities.Identity
 {
     public class AppUser : IdentityUser
     {
-        public string Id { get; set; }
+        public string DisplayName { get; set; }
         public Address Address { get; set; }
     }
 }
---
+++ EntityFramework.md
@@ -0,0 +1,6 @@
+# Entity Framework Commands
+
+### Identity
+
+
+`dotnet ef migrations add IdentityInitial -p Infrastructure -s API -c AppIdentityDbContext -o Identity/Migrations`
