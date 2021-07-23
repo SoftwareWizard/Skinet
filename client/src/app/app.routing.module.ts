f3e83a2e2eabb3058ede4a58fe8c2b68eac089e3
Simon Bauer <simon.bauer@doka.com>
2021-07-23 16:02:39 -0500

    add checkout modul, component

--- client/src/app/app.routing.module.ts
+++ client/src/app/app.routing.module.ts
@@ -6,12 +6,25 @@ import { Route, RouterModule, Routes } from '@angular/router';
 import { TestErrorComponent } from './core/test-error/test-error.component';
 
 const routes: Routes = [
-   { path: '', component: HomeComponent, data: { breadcrumb: 'Home'} },
-   { path: 'test-error', component: TestErrorComponent, data: { breadcrumb: 'Test Errors'} },
-   { path: 'server-error', component: ServerErrorComponent, data: { breadcrumb: 'Server Error'} },
-   { path: 'not-found', component: NotFoundComponent, data: { breadcrumb: 'Not Found'} },
-   { path: 'shop', loadChildren: () => import('./shop/shop.module').then(m => m.ShopModule), data: { breadcrumb: 'Shop'} },
-   { path: 'basket', loadChildren: () => import('./basket/basket.module').then(m => m.BasketModule), data: { breadcrumb: 'Basket'} },
+   { path: '', component: HomeComponent, data: { breadcrumb: 'Home' } },
+   { path: 'test-error', component: TestErrorComponent, data: { breadcrumb: 'Test Errors' } },
+   { path: 'server-error', component: ServerErrorComponent, data: { breadcrumb: 'Server Error' } },
+   { path: 'not-found', component: NotFoundComponent, data: { breadcrumb: 'Not Found' } },
+   {
+      path: 'shop',
+      loadChildren: () => import('./shop/shop.module').then(m => m.ShopModule),
+      data: { breadcrumb: 'Shop' },
+   },
+   {
+      path: 'basket',
+      loadChildren: () => import('./basket/basket.module').then(m => m.BasketModule),
+      data: { breadcrumb: 'Basket' },
+   },
+   {
+      path: 'checkout',
+      loadChildren: () => import('./checkout/checkout.module').then(m => m.CheckoutModule),
+      data: { breadcrumb: 'Checkout' },
+   },
    { path: '**', redirectTo: 'not-found', pathMatch: 'full' },
 ];
 
