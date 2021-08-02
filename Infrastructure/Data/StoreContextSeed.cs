e3a039e1d0002a710d379901abacc5230d810660
Simon Bauer <simon@software-wizard.tech>
2021-08-02 16:59:35 -0500

    optimize Seeding

--- Infrastructure/Data/StoreContextSeed.cs
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -6,6 +6,7 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using Core.Entities;
 using Core.Entities.OrderAggregate;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace Infrastructure.Data
@@ -14,48 +15,34 @@ namespace Infrastructure.Data
     {
         public static async Task Seed(StoreContext context, ILoggerFactory loggerFactory)
         {
+            var logger = loggerFactory.CreateLogger<StoreContextSeed>();
+
             try
             {
-                if (!context.ProductBrands.Any())
-                {
-                    var items = await LoadItems<ProductBrand>("brands.json");
-                    await context.ProductBrands.AddRangeAsync(items!);
-                    await context.SaveChangesAsync();
-                }
-
-                if (!context.ProductTypes.Any())
-                {
-                    var items = await LoadItems<ProductType>("types.json");
-                    await context.ProductTypes.AddRangeAsync(items!);
-                    await context.SaveChangesAsync();
-                }
-
-                if (!context.Products.Any())
-                {
-                    var items = await LoadItems<Product>("products.json");
-                    await context.Products.AddRangeAsync(items!);
-                    await context.SaveChangesAsync();
-                }
-
-                if (!context.DeliveryMethods.Any())
-                {
-                    var items = await LoadItems<DeliveryMethod>("delivery.json");
-                    await context.DeliveryMethods.AddRangeAsync(items!);
-                    await context.SaveChangesAsync();
-                }
+                await SeedItems<ProductBrand>("brands.json", context, logger);
+                await SeedItems<ProductType>("types.json", context, logger);
+                await SeedItems<Product>("products.json", context, logger);
+                await SeedItems<DeliveryMethod>("delivery.json", context, logger);
             }
             catch (Exception exception)
             {
-                var logger = loggerFactory.CreateLogger<StoreContextSeed>();
                 logger.LogError(exception, "Error while seeding");
             }
         }
 
-        private static async Task<List<TEntity>> LoadItems<TEntity>(string filename)
+        private static async Task SeedItems<TEntity>(string filename, DbContext context, ILogger logger) 
+            where TEntity : EntityBase
         {
-            var filepath = $"../Infrastructure/Data/SeedData/{filename}";
-            var data = await File.ReadAllTextAsync(filepath);
-            return JsonSerializer.Deserialize<List<TEntity>>(data);
+            if (!context.Set<TEntity>().Any())
+            {
+                var filepath = $"../Infrastructure/Data/SeedData/{filename}";
+                var data = await File.ReadAllTextAsync(filepath);
+                var items = JsonSerializer.Deserialize<List<TEntity>>(data);
+
+                await context.Set<TEntity>().AddRangeAsync(items!);
+                await context.SaveChangesAsync();
+                logger.LogWarning($"Seeded {items.Count} {typeof(TEntity)}(s)");
+            }
         }
     }
 }
