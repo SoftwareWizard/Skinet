71ebea87bbae88a68c39f58c1787f62ce22523ee
Simon Bauer <simon@software-wizard.tech>
2021-08-05 17:04:52 -0500

    implement orders component

--- client/src/app/orders/orders/orders.component.ts
+++ client/src/app/orders/orders/orders.component.ts
@@ -1,15 +1,26 @@
+import { OrdersService } from './../orders.service';
 import { Component, OnInit } from '@angular/core';
+import { IOrder } from 'src/app/shared/models/order';
 
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
-  styleUrls: ['./orders.component.scss']
+  styleUrls: ['./orders.component.scss'],
 })
 export class OrdersComponent implements OnInit {
+  orders: IOrder[] = [];
 
-  constructor() { }
+  constructor(private ordersService: OrdersService) {}
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    await this.getOrders();
   }
 
+  private async getOrders() {
+    try {
+      this.orders = await this.ordersService.getOrdersForUser();
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
