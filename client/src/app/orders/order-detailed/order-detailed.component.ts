d821d6383832882797985d2afd316d0ba446f5f6
Simon Bauer <simon@software-wizard.tech>
2021-08-05 17:35:16 -0500

    implement components

--- client/src/app/orders/order-detailed/order-detailed.component.ts
+++ client/src/app/orders/order-detailed/order-detailed.component.ts
@@ -1,15 +1,34 @@
+import { OrdersService } from './../orders.service';
+import { BreadcrumbService } from 'xng-breadcrumb';
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { IOrder } from 'src/app/shared/models/order';
 
 @Component({
   selector: 'app-order-detailed',
   templateUrl: './order-detailed.component.html',
-  styleUrls: ['./order-detailed.component.scss']
+  styleUrls: ['./order-detailed.component.scss'],
 })
 export class OrderDetailedComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  order: IOrder = null!;
+  constructor(
+    private route: ActivatedRoute,
+    private breadcrumbService: BreadcrumbService,
+    private ordersService: OrdersService
+  ) {
+    this.breadcrumbService.set('@OrderDetailed', '');
   }
 
+  async ngOnInit(): Promise<void> {
+    try {
+      const orderId = +(this.route.snapshot.paramMap.get('id') ?? 0);
+      this.order = await this.ordersService.getOrderDetailed(orderId);
+      this.breadcrumbService.set(
+        '@OrderDetailed',
+        `Order# ${this.order.id} - ${this.order.status}`
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
